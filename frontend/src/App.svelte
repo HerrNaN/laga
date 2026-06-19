90ccb28b58ad8d5a3944911e0b5de8eec7d07f31
Karl Wikström <karl@wikstrom.io>
2026-06-19 08:20:04 +0200

    chore: move list into its own dir and component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-    import { items } from "./lib/store";
-    import AddItem from "./lib/AddItem.svelte";
-    import ItemList from "./lib/ItemList.svelte";
-    import { pwaHack } from "./lib/utils/pwa-hack";
+    import List from "./lib/list/List.svelte";
+    import { pwaHack } from "./lib/list/utils/pwa-hack";
 </script>
 
 <main {@attach pwaHack("100dvh")}>
-    <header>
-        <h1>Inköpslista</h1>
-    </header>
-    <div class="list-area">
-        <ItemList items={$items} />
-    </div>
-    <AddItem />
+    <List />
 </main>
 
 <style>
@@ -26,28 +18,5 @@
         display: flex;
         flex-direction: column;
         background-color: var(--wa-color-surface-default);
-    }
-
-    .list-area {
-        flex: 1;
-        overflow-y: auto;
-        padding-bottom: var(--add-item-height);
-        background-color: var(--wa-color-surface-lowered);
-    }
-
-    header {
-        z-index: 1;
-        position: sticky;
-        top: 0;
-        padding: var(--wa-space-m);
-        background-color: var(--wa-color-surface-raised);
-        box-shadow: 0 var(--wa-shadow-offset-x-m) var(--wa-shadow-offset-y-m)
-            var(--wa-color-shadow);
-    }
-
-    h1 {
-        margin: 0;
-        font-size: var(--wa-font-size-xl);
-        color: var(--wa-color-text-normal);
     }
 </style>
---
+++ frontend/src/lib/list/List.svelte
@@ -0,0 +1,38 @@
+<script>
+    import AddItem from "./AddItem.svelte";
+    import ItemList from "./ItemList.svelte";
+    import { items } from "./store";
+</script>
+
+<header>
+    <h1>Inköpslista</h1>
+</header>
+<div class="list-area">
+    <ItemList items={$items} />
+</div>
+<AddItem />
+
+<style>
+    .list-area {
+        flex: 1;
+        overflow-y: auto;
+        padding-bottom: var(--add-item-height);
+        background-color: var(--wa-color-surface-lowered);
+    }
+
+    header {
+        z-index: 1;
+        position: sticky;
+        top: 0;
+        padding: var(--wa-space-m);
+        background-color: var(--wa-color-surface-raised);
+        box-shadow: 0 var(--wa-shadow-offset-x-m) var(--wa-shadow-offset-y-m)
+            var(--wa-color-shadow);
+    }
+
+    h1 {
+        margin: 0;
+        font-size: var(--wa-font-size-xl);
+        color: var(--wa-color-text-normal);
+    }
+</style>
